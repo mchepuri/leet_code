1d1bd137e3a4b3d51aaff8b7fcc2c7bee1eccda3
Muralimanohar <mchepuri@Mac.attlocal.net>
2026-02-20 23:21:50 -0800

    debounce and timelimit;

---
+++ src/debounce.js
@@ -0,0 +1,13 @@
+var debounce = function(fn, t) {
+    let timerId = null;
+
+    return function(...args) {
+        if (timerId !== null) {
+            clearTimeout(timerId);
+        }
+
+        timerId = setTimeout(() => {
+            fn(...args);
+        }, t);
+    }
+};
